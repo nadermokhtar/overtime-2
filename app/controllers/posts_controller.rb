541df3041c9400520589444ffa2e3a40357a0c22
nadermokhtar <nader.mokhtar@gmail.com>
2022-09-16 06:30:35 -0400

    saving work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,21 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:date, :rationale))
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,14 +1,28 @@
 require 'rails_helper'
 describe 'naviagete'do
-  describe 'post#index' do
+  describe 'index' do
     it 'can be reached successfully' do
       visit posts_path
       expect(page.status_code).to eq(200)
     end
 
-    it 'can be reached successfully' do
+    it 'can be reached has the correct content' do
       visit posts_path
       expect(page).to have_content(/Posts/)
     end
   end
+  describe 'creation' do
+    it 'can be reached successfully' do
+      visit new_post_path
+      expect(page.status_code).to eq(200)
+    end
+    it 'it can be created new form page' do
+      visit new_post_path
+      fill_in 'post[date]', with:Date.today
+      fill_in 'post[rationale]', with:'some narly rationale'
+      click_on 'Save'
+      expect(page).to have_content(/some narly rationale/)
+    end
+  end
+
 end
